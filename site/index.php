f2f459b0625960ad2c0c767995dece13f9e03e8c
bigslava <slava_vag@mail.ru>
2018-05-31 11:33:21 +0700

    init

---
+++ site/index.php
@@ -0,0 +1,8 @@
+
+<div class="site-about container">
+    <h1>About</h1>
+
+    <p>This is the About page. You may modify the following file to customize its content:</p>
+
+    <code><?= __FILE__ ?></code>
+</div>
---
+++ site/main.php
@@ -0,0 +1,11 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Main</title>
+</head>
+<body>
+<h1>Главная страница</h1>
+</body>
+</html>
