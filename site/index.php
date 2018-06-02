74372276fe01e5031742e345ce3ab143b0190630
bigslava <slava_vag@mail.ru>
2018-06-02 12:54:52 +0700

    change github

--- site/index.php
+++ site/index.php
@@ -1,14 +1,13 @@
 <?php
 	echo "Good Bay";
-
+?><br>
+<?php
 	echo "Good Bay new branch and new_f";
-
+?><br>
+<?php
 	echo "Hello old branch";
-
-	echo "Hello, Hello, Hello";
-
-
-	
-	
+?><br>
+<?php
+	echo "Hello, Hello, Hello";	
 ?>
 
